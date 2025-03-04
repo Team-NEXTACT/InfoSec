81a7627f95d7e6c48dbf0dd9936121e5857f9cf8
BlanC <74227460+kihong1602@users.noreply.github.com>
2025-03-05 01:53:11 +0900

    Loading Page 추가

--- app/src/components/StartButton.jsx
+++ app/src/components/StartButton.jsx
@@ -1,38 +1,25 @@
 import React from 'react';
 import {Button} from '@mui/material';
 import {useNavigate} from 'react-router-dom';
-import {getAuth, GoogleAuthProvider, signInWithPopup} from "firebase/auth";
-import app from "../firebaseConfig";
+import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
+import app from '../firebaseConfig';
 
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider(auth);
 provider.addScope('https://www.googleapis.com/auth/gmail.readonly');
 
-const fetchEmails = (accessToken, refreshToken, email) => {
-  fetch('http://127.0.0.1:5001/pdf-security/us-central1/api/emails', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({accessToken, refreshToken, email}),
-  });
-}
-
 const StartButton = () => {
   const navigate = useNavigate();
 
   const handleLogin = async () => {
-    // TODO: 구글 OAuth 로그인 구현
     try {
       const result = await signInWithPopup(auth, provider);
       const credential = GoogleAuthProvider.credentialFromResult(result);
-
       const accessToken = credential.accessToken;
       const refreshToken = result.user.refreshToken;
       const email = result.user.email;
 
-      fetchEmails(accessToken, refreshToken, email);
-      navigate('/dashboard');
+      navigate('/loading', {state: {accessToken, refreshToken, email}}); // 로딩 페이지로 이동
     } catch (error) {
       console.error(`로그인 실패: ${error}`);
     }
@@ -66,4 +53,4 @@ const StartButton = () => {
   );
 };
 
-export default StartButton; 
+export default StartButton;
